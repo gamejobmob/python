2db067be2915a6c4e9e58c43db33325597e77981
gamejobmob <86462374+gamejobmob@users.noreply.github.com>
2021-08-27 15:31:06 +0900

    210827

---
+++ if-3개중 큰값.py
@@ -0,0 +1,61 @@
+"""
+정수 3개를 입력받아 그중 가장큰수를 출력하는 프로그램
+max_data
+"""
+
+num1 = int(input("1번째 수"))
+num2 = int(input("2번째 수"))
+num3 = int(input("3번째 수"))
+
+if num1 > num2 :
+  max_data = num1
+else :
+  max_data = num2
+
+if num3 > max_data :
+  max_data = num3
+
+print("max_data = %d" %(max_data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+rm snrnqhsk Qkfmrp
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
