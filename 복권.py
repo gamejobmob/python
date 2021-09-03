df309d0757a1472e7007fce52dff6c7a360cea5c
gamejobmob <86462374+gamejobmob@users.noreply.github.com>
2021-09-03 15:51:45 +0900

    Add files via upload

---
+++ 복권.py
@@ -0,0 +1,34 @@
+
+import random
+
+random.randrange(1,21)
+
+num1 = int(input("복권1")
+num2 = int(input("복권2")
+num3 = int(input("복권3")
+num4 = int(input("복권4")
+num5 = int(input("복권5")
+num6 = int(input("복권6")
+
+if num1 or num2 or num3 or num4 or num5 or num6 > 20 :
+           print("1부터 20 사이의 숫자로만 정해주세요")
+           exit()
+elif num1 or num2 or num3 or num4 or num5 or num6 < 1 :
+  print("1부터 20 사이의 숫자로만 정해주세요")
+  exit()
+  
+print(
+
+num1-1 = randrange(1,21)
+num2-1 = randrange(1,21)
+num3-1 = randrange(1,21)
+num4-1 = randrange(1,21)
+num5-1 = randrange(1,21)
+num6-1 = randrange(1,21)
+
+
+
+
+
+
+  
