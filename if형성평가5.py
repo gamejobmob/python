68691aad9d97581cf0a0661a04cf253565b48778
gamejobmob <86462374+gamejobmob@users.noreply.github.com>
2021-09-10 15:34:34 +0900

    210910

---
+++ if형성평가5.py
@@ -0,0 +1,18 @@
+"""
+if형성평가5.py
+1 - 12 사이의 정수를 입력받아 입력받은 월의 날수 말하기
+ex) 1월은 31일 입니다
+"""
+month = int(input("당신의 월은?"))
+
+if month == month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12 :
+    print("%d 월은 31일 입니다" %(month))
+elif month == month == 4 or month == 6 or month == 9 or month == 11 :
+    print("%d 월은 30일 입니다" %(month))
+elif month == month == 2 :
+    print("%d 월은 28일 입니다" %(month))
+else :
+    print("없는 월 입니다")
+
+                  
+                
