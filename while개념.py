7ceb1b8d50c9cb9d381ea0e8b2972fce085709b2
gamejobmob <86462374+gamejobmob@users.noreply.github.com>
2021-09-17 14:26:05 +0900

    Add files via upload

---
+++ while개념.py
@@ -0,0 +1,15 @@
+"""
+while개념.py
+while :~하는 동안에
+반복문
+while 조건 :
+  조건이 참일동안 반복
+"""
+# 안녕하세요 라는 말을 1000번 출력하는 프로그램
+
+#count 줄여서 cnt
+
+cnt = 0
+while cnt < 1000 :
+    print("안녕하세요")
+    cnt = cnt + 1
