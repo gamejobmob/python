29dfe629f0829d895f7e3b1f90de140f0d167025
gamejobmob <86462374+gamejobmob@users.noreply.github.com>
2021-09-17 15:14:05 +0900

    210917

---
+++ while무한반복.py
@@ -0,0 +1,14 @@
+"""
+whlie무한반복.py
+while의 조건이 계속 참이면 무한반복
+조건 : True/false
+True : 게속 무한 반복
+False : 계속 거짓 > 한번도 실행되지 않는다
+break : while에서 탈출하는 명령어
+"""
+while False :
+    print("while 무한거짓")
+    break
+while True :
+    print("while 무한반복")
+    break
